e44f16bc13c549936fb789e831c8bac57071fe33
josetruyolm <jostrum85@gmail.com>
2023-04-19 10:56:51 -0500

    nuevas lineas de codigo

---
+++ script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.modal')
+const button1 = document.querySelector('butto1')
+const closeButton = document.querySelector('modal-container-close')
+
+button1.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+})
+
+closeButton.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    modal.classList.remove('visible')
+})
